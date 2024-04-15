32768bb209de4828c2447449563e2abdfe124c0a
Tushar Pachouri <tusharpachouri001@gmail.com>
2024-04-15 20:39:56 +0530

    Add new components and update index.jsx

---
+++ src/components/index.jsx
@@ -0,0 +1,9 @@
+import Login from "./Login/Login.jsx";
+import Home from "./Home/Home.jsx";
+import Nav from "./NavBar/Nav.jsx";
+import Footer from "./Footer/Footer.jsx";
+import SignUp from "./SignUp/SignUp.jsx";
+import Contact from "./ContactUs/Contact.jsx";
+import AboutUs from "./AboutUs/AboutUs.jsx";
+
+export { Login, Home, Nav , Footer, SignUp, Contact, AboutUs};
