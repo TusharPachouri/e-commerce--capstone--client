eeae0539c753db1bfbce2ee34bfe7f279f82581b
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-04-29 19:47:34 +0530

    successfully fetched products api and listed items on the product route

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,87 +1,50 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom';
 export default function Products() {
+    const [items, setItems] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get('https://e-commerce-capstone-server.vercel.app/api/v1/products/');
+                setItems(response.data.data.products);
+                console.log(items);
+            } catch (error) {
+                setError('Error fetching data from the server.');
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchProducts();
+    }, []);
+
     return (
         <>
             <section className="py-6 sm:py-12 bg-gray-800 text-gray-100">
                 <div className="container p-6 mx-auto space-y-8">
                     <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-4">
                         {/* item 1 */}
-                        <article className="flex flex-col bg-gray-900">
-                            <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
-                                <img alt="" className="object-cover w-full h-100 bg-gray-500" src="https://m.media-amazon.com/images/I/81vxWpPpgNL._SL1500_.jpg" />
-                            </a>
-                            <div className="flex flex-col flex-1 p-6">
-                                <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
-                                <a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline text-violet-400">Samsung Galaxy S24 Ultra 5G</a>
-                                <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">Meet Galaxy S24 Ultra, the ultimate form of Galaxy Ultra with a new titanium exterior.</h3>
-                                <div className="flex justify-between pt-3 space-x-2 text-xs text-gray-400">
-                                    <button type="button" className="flex-1 p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50">
-                                        Rent
-                                    </button>
-                                    <button type="button" className="flex-1 p-3 font-semibold tracking-wide rounded-md bg-gray-700 text-white dark:bg-gray-600 dark:text-gray-50">
-                                        Buy Now
-                                    </button>
+                        {/* I've to loop this card */}
+                        {items.map((product) => (
+                            <article key={product._id} className="flex flex-col bg-gray-900">
+                                <a rel="noopener noreferrer" href="#" aria-label={product.item_name}>
+                                    <img alt={product.item_name} className="object-cover w-full h-100 bg-gray-500" src={product.image} />
+                                </a>
+                                <div className="flex flex-col flex-1 p-6">
+                                    <a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline text-violet-400">{product.item_name}</a>
+                                    <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">{product.description}</h3>
+                                    <div className="flex justify-between pt-3 space-x-2 text-xs text-gray-400">
+                                        <Link to={`/rent/${product._id}`} className="flex-1 p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50">
+                                            Rent
+                                        </Link>
+                                        <Link to={`/buy/${product._id}`} className="flex-1 p-3 font-semibold tracking-wide rounded-md bg-gray-700 text-white dark:bg-gray-600 dark:text-gray-50">
+                                            Buy Now
+                                        </Link>
+                                    </div>
                                 </div>
-                            </div>
-                        </article>
-                        {/* item 2 */}
-                        <article className="flex flex-col bg-gray-900">
-                            <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
-                                <img alt="" className="object-cover w-full h-100 bg-gray-500" src="https://m.media-amazon.com/images/I/61mI-ghw8mL._SL1200_.jpg" />
-                            </a>
-                            <div className="flex flex-col flex-1 p-6">
-                                <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
-                                <a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline text-violet-400">ASUS ROG Flow X13</a>
-                                <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">We fine-tuned every aspect of cooling system. with liquid metal on CPU to Keep optimum temperature.</h3>
-                                <div className="flex justify-between pt-3 space-x-2 text-xs text-gray-400">
-                                    <button type="button" className="flex-1 p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50">
-                                        Rent
-                                    </button>
-                                    <button type="button" className="flex-1 p-3 font-semibold tracking-wide rounded-md bg-gray-700 text-white dark:bg-gray-600 dark:text-gray-50">
-                                        Buy Now
-                                    </button>
-                                </div>
-                            </div>
-                        </article>
-                        {/* item 3 */}
-                        <article className="flex flex-col bg-gray-900">
-                            <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
-                                <img alt="" className="object-cover w-full h-100 bg-gray-500" src="https://m.media-amazon.com/images/I/61mOVGinDdL._SL1500_.jpg" />
-                            </a>
-                            <div className="flex flex-col flex-1 p-6">
-                                <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
-                                <a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline text-violet-400">Daikin 0.8 Ton 3 Star</a>
-                                <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">Split AC with non-inverter compressor: Power chill operation to ensure quick cooling</h3>
-                                <div className="flex justify-between pt-3 space-x-2 text-xs text-gray-400">
-                                    <button type="button" className="flex-1 p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50">
-                                        Rent
-                                    </button>
-                                    <button type="button" className="flex-1 p-3 font-semibold tracking-wide rounded-md bg-gray-700 text-white dark:bg-gray-600 dark:text-gray-50">
-                                        Buy Now
-                                    </button>
-                                </div>
-                            </div>
-                        </article>
-                        {/* // item 4 */}
-                        <article className="flex flex-col bg-gray-900">
-                            <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
-                                <img alt="" className="object-cover w-full h-100 bg-gray-500" src="https://m.media-amazon.com/images/I/71657TiFeHL._SL1500_.jpg" />
-                            </a>
-                            <div className="flex flex-col flex-1 p-6">
-                                <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
-                                <a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline text-violet-400">Apple iPhone 15</a>
-                                <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">We fine-tuned every aspect of cooling system. with liquid metal on CPU to Keep optimum temperature.</h3>
-                                <div className="flex justify-between pt-3 space-x-2 text-xs text-gray-400">
-                                    <button type="button" className="flex-1 p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50">
-                                        Rent
-                                    </button>
-                                    <button type="button" className="flex-1 p-3 font-semibold tracking-wide rounded-md bg-gray-700 text-white dark:bg-gray-600 dark:text-gray-50">
-                                        Buy Now
-                                    </button>
-                                </div>
-                            </div>
-                        </article>
+                            </article>
+                        ))}
                     </div>
                 </div>
             </section>
