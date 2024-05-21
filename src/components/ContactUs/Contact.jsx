67129c7705118afcb844792b9dae1f4698f5fdc7
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-05-21 09:39:41 +0530

    chore: Update Contact component UI and improve form styling
    This commit updates the UI of the Contact component by applying a new layout and styling. The section is now centered on the screen and has a dark background. The form fields have been restyled with rounded corners and a darker background color. These changes aim to enhance the visual appeal and user experience of the Contact page.

--- src/components/ContactUs/Contact.jsx
+++ src/components/ContactUs/Contact.jsx
@@ -41,97 +41,84 @@ const Contact = () => {
   };
 
   return (
-    <section className="min-h-screen dark:bg-gray-800 dark:text-gray-300">
-      <div className="w-full mx-auto flex justify-center items-center">
-        <div className="max-w-md w-full rounded-lg overflow-hidden shadow-lg bg-gray-800">
-          <div className="flex justify-between bg-gray-700 text-white p-4">
-            <div className="flex items-center space-x-2">
-              <div className="w-3 h-3 bg-red-500 rounded-full"></div>
-              <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
-              <div className="w-3 h-3 bg-green-500 rounded-full"></div>
-            </div>
-            <div className="flex items-center space-x-2">
-              <div className="w-3 h-3 bg-white rounded-full"></div>
-              <div className="w-3 h-3 bg-white rounded-full"></div>
-              <div className="w-3 h-3 bg-white rounded-full"></div>
-            </div>
+    <section className="flex justify-center items-center min-h-screen py-6 sm:py-12 bg-gray-800 text-gray-100">
+<div className="flex flex-col max-w-3xl p-6 space-y-4 sm:p-10 bg-gray-900 text-gray-100">
+        <div className="py-6 md:py-0 md:px-6">
+          <h1 className="text-4xl font-bold">Get in touch</h1>
+          <p className="pt-2 pb-4">Fill in the form to start a conversation</p>
+          <div className="space-y-4">
+            <p className="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
+                <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"></path>
+              </svg>
+              <span>Lovely Professiobal University</span>
+            </p>
+            <p className="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
+                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
+              </svg>
+              <span>+91 987654321</span>
+            </p>
+            <p className="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
+                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+              </svg>
+              <span>userdetail@gmail.com</span>
+            </p>
           </div>
-          <div className="p-6">
-            <h1 className="text-3xl font-bold text-center mb-4 text-white">
-              CONTACT US
-            </h1>
-            <form className="space-y-4" onSubmit={handleSubmit}>
-              <div className="flex flex-col">
-                <label htmlFor="name" className="text-sm font-medium text-gray-300">
-                  Name
-                </label>
-                <input
+        </div>
+            <form className="flex flex-col py-6 space-y-6 md:py-0 md:px-6" onSubmit={handleSubmit}>
+                <label htmlFor="name" className="block">
+                  <span className="mb-1">Full Name</span>
+                  <input
                   type="text"
                   id="name"
-                  className="input-field text-gray-800"
+                  className="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 bg-gray-800"
                   value={formData.name}
                   onChange={handleChange}
                 />
-              </div>
-              <div className="flex flex-col">
-                <label htmlFor="email" className="text-sm font-medium text-gray-300">
-                  Email
+
                 </label>
-                <input
+                <label htmlFor="email" className="block">
+                  <span className="mb-1">Email address</span>
+                  <input
                   type="email"
                   id="email"
-                  className="input-field text-gray-800"
+                  className="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 bg-gray-800"
                   value={formData.email}
                   onChange={handleChange}
                 />
-              </div>
-              <div className="flex flex-col">
+                </label>
                 <label
                   htmlFor="contactNumber"
-                  className="text-sm font-medium text-gray-300"
+                  className="block"
                 >
-                  Contact No
-                </label>
-                <input
+                  <span className="mb-1">Contact Number</span>
+                  <input
                   type="text"
                   id="contactNumber"
-                  className="input-field text-gray-800"
+                  className="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 bg-gray-800"
                   value={formData.contactNumber}
                   onChange={handleChange}
                 />
-              </div>
-              <div className="flex flex-col">
+                </label>
+
                 <label
                   htmlFor="message"
-                  className="text-sm font-medium text-gray-300"
+                  className="block"
                 >
-                  Message
-                </label>
-                <textarea
+                  <span className="mb-1">Message</span>
+                  <textarea
                   id="message"
-                  className="input-field h-32 text-gray-800"
+                  className="block w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 bg-gray-800"
                   value={formData.message}
                   onChange={handleChange}
                 ></textarea>
-              </div>
-              <div className="flex justify-end space-x-4">
-                <button
-                  type="button"
-                  className="cancel-button"
-                >
-                  CANCEL
-                </button>
-                <button
-                  type="submit"
-                  className="send-button"
-                >
-                  SEND
-                </button>
-              </div>
+                </label>
+                <button type="submit" className="self-center px-8 py-3 text-lg rounded focus:ring hover:ring focus:ring-opacity-75 dark:bg-violet-600 dark:text-gray-50 focus:dark:ring-violet-600 hover:dark:ring-violet-600">Submit</button>
             </form>
-          </div>
         </div>
-      </div>
       <ToastContainer />
     </section>
   );
